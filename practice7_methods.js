e892941134e8b1792bb8c3e2954127dd6d92b222
Sergey <sergey.zakharchenko@live.com>
2023-05-18 16:01:20 -0400

    practice JS

---
+++ practice7_methods.js
@@ -0,0 +1,30 @@
+
+// ------------- 5/18/2023 ---------------
+
+// function to print: 
+
+function print() {
+    console.log("Hello World of JS");
+}
+
+// create function that accepts one parameter and print out hello user +
+// whatever you passed from the param
+
+function sayHelloTo(name){
+    console.log("Hello ::: " + name);
+ 
+}
+
+// create a function that accept 2 numbers
+//and return the addition results of 2 numbers
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+
+
+print () ;
+
+sayHelloTo('Sergey Zakharchenko');
+sayHelloTo('Yelena');
+
+console.log( addNumbers(100, 200) );
