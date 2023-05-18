d33fa0a9b48ae0200714849ce4cb3e15aae14a46
Sergey <sergey.zakharchenko@live.com>
2023-05-18 15:14:53 -0400

    practice JS

---
+++ practice6_destructing.js
@@ -0,0 +1,26 @@
+
+// -------------- 5/18/2023 --------------
+
+
+let names = ["Clark","Bruce", "Barry", "Diana", "John"];
+// store the first item and second item into a variable separately
+
+// this way:
+// let firstItem = names[0];
+// let secondItem = names[1];
+
+// or this way in one liner: this called destructing
+//below code will take the first item and second item in names array
+let [firstItem, secondItem, x] = names; 
+
+
+console.log(firstItem);
+console.log(secondItem);
+console.log(x);
+
+
+
+
+
+
+
